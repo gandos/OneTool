f74b9a6d164924e1965386e30524cd1a64340557
Rino Well <Rino Well>
2017-03-12 03:53:46 +0800

    drag drop

---
+++ src/main/java/id/web/gandos/domain/PcapHttpSummary.java
@@ -0,0 +1,7 @@
+package id.web.gandos.domain;
+
+/**
+ * Created by rinow on 3/12/2017.
+ */
+public class PcapHttpSummary {
+}
---
+++ src/main/java/id/web/gandos/service/HttpService.java
@@ -0,0 +1,7 @@
+package id.web.gandos.service;
+
+/**
+ * Created by rinow on 3/11/2017.
+ */
+public class HttpService {
+}
